8906fbf0495bb82c0993487b5bcae00b5ecbaf39
songseungkwan <ssk3553@naver.com>
2023-05-24 15:37:34 +0900

    0524

---
+++ src/com/mycompany/firstJava/ch02/Hellojava.java
@@ -0,0 +1,7 @@
+package com.mycompany.firstJava.ch02;
+
+public class Hellojava {
+
+		
+	
+}
---
+++ src/com/mycompany/firstJava/ch02/Helloworld.java
@@ -0,0 +1,23 @@
+package com.mycompany.firstJava.ch02;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWorld");
+		System.out.println("송승관");
+		System.out.println("930914");
+		System.out.println("31");
+		System.out.println("173");
+		System.out.println("73");
+		System.out.println("경기도");
+		System.out.println("김포시");
+		System.out.println("양촌읍");
+		System.out.println("123");
+		System.out.println("456");
+		System.out.println("789");
+		
+	}
+
+}
