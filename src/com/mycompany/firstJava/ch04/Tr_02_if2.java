f5084e0ed62cc1d80fe254c39638eecfddbecf1b
songseungkwan <ssk3553@naver.com>
2023-05-31 17:39:49 +0900

    0531

--- src/com/mycompany/firstJava/ch04/Tr_02_if2.java
+++ src/com/mycompany/firstJava/ch04/Tr_02_if2.java
@@ -12,9 +12,9 @@ public class Tr_02_if2 {
 		// 일반 5% , 브론즈 10% , 실버 15% , 골드 20% , 플래티넘 2.7% 할인율 
 		
 		if(grade == 1) {
-			salesCost = cost * (1-0.05); {
-				System.out.println("정가가" + cost + "원인 일반 등급의 할인가는" + (int)salesCost + "원 입니다.");
-			}
+			salesCost = cost * (1-0.05); 
+			System.out.println("정가가" + cost + "원인 일반 등급의 할인가는" + (int)salesCost + "원 입니다.");
+			
 		} else if (grade == 2) {
 			salesCost = cost * (1-0.1); {
 				
